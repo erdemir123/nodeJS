de39d328bebf9f3fe5f079b0ef00626bbe5c4eaa
sadik <sadikerdemir123@icloud.com>
2024-03-03 23:27:30 +0300

    express routing

---
+++ inClass/node_routes/index.js
@@ -0,0 +1,15 @@
+"use strict"
+require("dotenv").config
+
+const PORT = process.env.PORT || 8000
+const HOST = process.env.HOST || "127.0.0.1"
+
+
+const express = require("express")
+const app = express()
+app.get("/",(req,res)=>{
+    res.send({
+        "message":"Hello"
+    })
+})
+app.listen(PORT)
