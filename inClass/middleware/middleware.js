1dd23073a09ab9ecc260fa4fe07ca00d90c6378d
sadik <sadikerdemir123@icloud.com>
2024-03-05 00:13:43 +0300

    middleware parametre

--- inClass/middleware/middleware.js
+++ inClass/middleware/middleware.js
@@ -4,5 +4,16 @@ const app = express()
 require("dotenv").config()
 const PORT =process.env.PORT
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res, next) => {
+    if (req.query.username === "sadik") {
+        req.welcome= "Welcome"
+        res.userName= req.query?.username
+        next();
+    } else {
+        res.send("something went wrong");
+    }
+});
+app.get('/', (req, res) => res.send({
+    "message": req.welcome + " " + res.userName
+}))
 app.listen(PORT, () => console.log(`Example app listening on PORT ${PORT}!`))
